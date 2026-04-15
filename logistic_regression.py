ad6740b7f0fa2b87c922becdbe30d0a30f57423e
Jackson Windhorst <axnjxn17@comcast.net>
2026-04-15 16:17:16 -0400

    finished up the code, will update dataset with something with more noise

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 class Logistic_Regression:
     def __init__(self, learning_rate = 0.05, epochs = 1000):
@@ -6,28 +7,36 @@ class Logistic_Regression:
         self.epochs = epochs
         self.theta = None
     
-    def sigmoid(self):
-        return 1 / (1 + np.exp(-self.theta))
+    def sigmoid(self, z):
+        return 1 / (1 + np.exp(-z))
 
     def fit(self, X, y):
         X = design_matrix(X)
         self.theta = np.zeros((X.shape[1], 1))
         y = y.reshape(-1, 1)
-        n = len(X)
         for _ in range(self.epochs):
-            pass
+            p = self.sigmoid(X @ self.theta)
+            error = p - y
+            gradient = X.T @ error
+            self.theta -= self.learning_rate * gradient
 
-    def pred(self):
-        pass
+    def pred(self, X):
+        X = design_matrix(X)
+        p = self.sigmoid(X @ self.theta)
+        return (p >= 0.5).astype(int)
 
 def design_matrix(X):
     column_ones = np.ones((X.shape[0], 1))
-    return np.hstack(column_ones, X)
+    return np.hstack((column_ones, X))
 
 
 if __name__ == '__main__':
-    X = 2 * np.random.rand()
-    y = 
+    np.random.seed(42)
+    X = 2 * np.random.rand(100, 1)
+    y = (X > 1).astype(int).flatten()
 
     model = Logistic_Regression()
     model.fit(X, y)
+
+    print(model.pred(X[:5]))
+    print(y[:5])
